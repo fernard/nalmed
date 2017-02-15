10a138a3d9d57844e95393a606d68eae6431f283
fernard <pawel.kubis1989@gmail.com>
2017-02-15 07:51:47 +0100

    First part

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+  var body = document.querySelector('body');
+
+  body.classList.remove('preload');
+  console.log('preload wywalony');
+
+});
