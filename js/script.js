7df70af70f2a4c02589c3f16824cd28d09580cd8
fernard <pawel.kubis1989@gmail.com>
2017-03-04 10:14:02 +0100

    Zmian cd

--- js/script.js
+++ js/script.js
@@ -3,9 +3,48 @@ $(document).ready(function() {
     // Clean CSS Animations
     $('body').removeClass('preload');
 
-    // Inline Menu Animation
+    // Header scroll
+
+    $(window).scroll(function() {
+        var scrollValue = $(this).scrollTop();
+
+        if (scrollValue > 0) {
+
+            $('header').addClass('onScroll');
+        } else {
+
+            $('header').removeClass('onScroll');
+        }
+    });
+
+    // Mobile-Menu Animation
+
+    var hambuger = $('.hamburger');
+    hambuger.on('click', function() {
+
+        $(this).toggleClass('extended');
+
+        $('.mobile-menu').animate({
+
+            height: 'toggle',
+            opacity: 'toggle'
+        }, 'quick');
+
+    });
+
+    //  Hide mobile-menu and reset hamburger class when mobile-menu is visible and hamburger class disappears due to size-change
+    $(window).on('resize', function() {
+        var windowWidth = $(this).innerWidth();
+        if (windowWidth > 992 && $('.hamburger').hasClass('extended')) {
+
+            $('.mobile-menu').css('display', 'none');
+            $('.hamburger').toggleClass('extended');
+        }
+
+    });
+
+    // Inline Menu 'slide to sections' animation
     var menuLinks = $('nav').find('li a');
-    console.log(menuLinks);
 
     menuLinks.on('click', function() {
 
