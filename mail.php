d9c723a90528e1f8e74711b39c4e3ff4702b513e
fernard <pawel.kubis1989@gmail.com>
2017-03-05 10:36:50 +0100

    Walidacja cd

--- mail.php
+++ mail.php
@@ -6,7 +6,8 @@
     $email = $_POST['formData.email'];
     $subject = $_POST['formData.subject'];
     $message = $_POST['formData.message'];
-
+    
+    header("Access-Control-Allow-Origin: *");
     mail("pawel.kubis1989@gmail.com", $name, $email, $subject, $message);
   }
 
