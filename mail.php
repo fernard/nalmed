8c3ca6b2ed671c4aa41080602e56c8cf154e74c9
fernard <pawel.kubis1989@gmail.com>
2017-03-05 12:03:25 +0100

    poprawki PHP

--- mail.php
+++ mail.php
@@ -1,12 +1,13 @@
 <?php
-
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
   if($_POST) {
 
     $name = $_POST['formData.name'];
     $email = $_POST['formData.email'];
     $subject = $_POST['formData.subject'];
     $message = $_POST['formData.message'];
-    
+
     header("Access-Control-Allow-Origin: *");
     mail("pawel.kubis1989@gmail.com", $name, $email, $subject, $message);
   }
