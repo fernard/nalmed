eae1bcb9d4e0851333e7741f11898611816aff2e
fernard <pawel.kubis1989@gmail.com>
2017-03-05 12:13:29 +0100

    poprawki PHP

--- mail.php
+++ mail.php
@@ -1,6 +1,7 @@
 <?php
-  header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+  header("Access-Control-Allow-Origin: *");
+
   if($_POST) {
 
     $name = $_POST['formData.name'];
