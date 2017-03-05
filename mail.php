722ff0f764a8b142175b3db88fce0e22f265098a
fernard <pawel.kubis1989@gmail.com>
2017-03-05 12:28:09 +0100

    poprawki PHP

--- mail.php
+++ mail.php
@@ -4,10 +4,10 @@
 
   if($_POST) {
 
-    $name = $_POST['formData.name'];
-    $email = $_POST['formData.email'];
-    $subject = $_POST['formData.subject'];
-    $message = $_POST['formData.message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
     mail("pawel.kubis1989@gmail.com", $name, $email, $subject, $message);
   }
