037cef43a88fb1c08708653b6143c063a9bd712d
fernard <pawel.kubis1989@gmail.com>
2017-03-17 10:07:46 +0100

    Workshops

--- js/script.js
+++ js/script.js
@@ -217,6 +217,7 @@ $(document).ready(function() {
 
                 }).done(function(success) {
                     console.log(success);
+                    // window.location.replace('http://www.wp.pl');
                     errorMessage.css('backgroundColor', 'green');
                     errorMessage.text('Udało się wysłać wiadomość!');
                     errorMessage.fadeIn('400');
@@ -233,6 +234,97 @@ $(document).ready(function() {
     });
 
     // Google Maps
+
+
+
+  var map;
+
+  function initMap() {
+      var myLatLng = {
+          lat: 52.1331211,
+          lng: 20.657210299999974
+      };
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: myLatLng,
+          styles: [{
+                  "featureType": "administrative",
+                  "elementType": "labels.text.fill",
+                  "stylers": [{
+                      "color": "#444444"
+                  }]
+              },
+              {
+                  "featureType": "landscape",
+                  "elementType": "all",
+                  "stylers": [{
+                      "color": "#f2f2f2"
+                  }]
+              },
+              {
+                  "featureType": "poi",
+                  "elementType": "all",
+                  "stylers": [{
+                      "visibility": "off"
+                  }]
+              },
+              {
+                  "featureType": "road",
+                  "elementType": "all",
+                  "stylers": [{
+                          "saturation": -100
+                      },
+                      {
+                          "lightness": 45
+                      }
+                  ]
+              },
+              {
+                  "featureType": "road.highway",
+                  "elementType": "all",
+                  "stylers": [{
+                      "visibility": "simplified"
+                  }]
+              },
+              {
+                  "featureType": "road.arterial",
+                  "elementType": "labels.icon",
+                  "stylers": [{
+                      "visibility": "off"
+                  }]
+              },
+              {
+                  "featureType": "transit",
+                  "elementType": "all",
+                  "stylers": [{
+                      "visibility": "off"
+                  }]
+              },
+              {
+                  "featureType": "water",
+                  "elementType": "all",
+                  "stylers": [{
+                          "color": "#46bcec"
+                      },
+                      {
+                          "visibility": "on"
+                      }
+                  ]
+              }
+          ],
+      });
+
+      var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Centrum Terapii Uzależnień i Zaburzeń Nalmed'
+      });
+  }
+
+
+initMap();
+
 
 
 
