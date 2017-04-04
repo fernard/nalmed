d8dd424de9040de32d164d5e2964085f065a7140
fernard <pawel.kubis1989@gmail.com>
2017-04-04 18:32:04 +0200

    Form validation

--- mail.php
+++ mail.php
@@ -1,15 +1,25 @@
 <?php
-  $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-  // header("Access-Control-Allow-Origin: *");
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+    $headers .= "From: biuro@nalmed.pl";
 
+  
   if($_POST) {
-    $to = 'pawel.kubis1989@gmail.com';
+    $to = 'iwona.kubis@nalmed.pl';
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
-    $message = 'Temat wiadomości: '.$subject.'<br> Od:'.$email.'<br> Treść wiadomości: '.$_POST['message'];
+    $message = '<!doctype html>
+                <html>
+                <head>
+                <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+                <title>'.$subject.'</title>
+                </head>
+                <body>
+                <h2>Nadawca: '.$email.'</h2>
+                <p><b>Treść wiadomości:</b><br>'.$_POST['message'].'</p>
+                </body></html>';
 
-    mail($to,$subject,$message);
+    mail($to,$subject,$message,$headers);
     $info = array('Status'=>'OK');
     echo json_encode($info);
   } else {
