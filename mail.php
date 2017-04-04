e00d8e9da35064cd548fd2a298a1eb9dae3529c0
fernard <pawel.kubis1989@gmail.com>
2017-04-04 12:20:03 +0200

    Form validation

--- mail.php
+++ mail.php
@@ -1,15 +1,17 @@
 <?php
-  header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+  $headers  = "Content-type: text/html; charset=utf-8 \r\n";
   // header("Access-Control-Allow-Origin: *");
 
   if($_POST) {
-
+    $to = 'pawel.kubis1989@gmail.com';
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    $message = 'Temat wiadomości: '.$subject.'<br> Od:'.$email.'<br> Treść wiadomości: '.$_POST['message'];
 
-    mail("pawel.kubis1989@gmail.com", $name, $email, $subject, $message);
-    $info = ['Status' => 'OK'];
+    mail($to,$subject,$message);
+    $info = array('Status'=>'OK');
     echo json_encode($info);
-  }
+  } else {
+  echo json_encode(array('Status'=>'Błąd'));
+}
